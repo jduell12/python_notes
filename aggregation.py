6474116c38b2bb1ed02eefbf6e4c8eb85660c965
Jessica <duellje@oregonstate.edu>
2020-09-02 19:52:33 -0400

    added tk composition notes

---
+++ aggregation.py
@@ -0,0 +1,36 @@
+"""
+Python Class aggregation
+
+Association:
+    Composition
+        has-a
+            there is a link between 2 classes
+    Aggregation
+        class A can exist independently of class B 
+        
+"""
+
+
+# class Restaurant:
+#     def __init__(self, name, staff, dinner_menu):
+#         self.name = name
+#         self.staff = Employees[]  # has an
+#         self.dinner_menu = Menu()  # has a
+#     # class functions
+
+
+# class Employee:
+#     def __init__(self, name, wage):
+#         self.name = name
+#         self.wage = wage
+
+#     def calc_pay(self, hours):
+#         return self.wage*hours
+
+
+# class Menu:
+#     def __init__(self, name):
+#         self.name = name
+#         self.dishes = Food[]
+#         self.drinks = Drinks[]
+#     # more attributes
